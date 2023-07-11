007e1f1eda0d7625503dc3df0b81b2b2bd188d48
함인규 <hig1207@midasin.com>
2023-07-11 14:28:46 +0900

    FIX :: jest path alias 설정

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    moduleNameMapper: {
+        '@/(.*)$': '<rootDir>/$1',
+        "@src/(.*)$": '<rootDir>/src/$1',
+        "@components/(.*)$": '<rootDir>/src/components/$1',
+        "@models/(.*)$": '<rootDir>/src/models/$1'
+    },
+};
